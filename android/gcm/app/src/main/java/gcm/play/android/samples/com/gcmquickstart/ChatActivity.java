043f9bb76a37fcbcc4aef23f6083e655ef4e1b7f
Jordan Winch <jwinch@hourlynerd.com>
2016-07-21 18:33:38 -0400

    chat will now load all saved push notifications on create, and update the window with new ones when they come in.

--- android/gcm/app/src/main/java/gcm/play/android/samples/com/gcmquickstart/ChatActivity.java
+++ android/gcm/app/src/main/java/gcm/play/android/samples/com/gcmquickstart/ChatActivity.java
@@ -1,21 +1,22 @@
 package gcm.play.android.samples.com.gcmquickstart;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Adapter;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * Created by jordanwinch on 7/21/16.
@@ -25,6 +26,14 @@ public class ChatActivity extends AppCompatActivity {
     private EditText currentMessage;
     private ImageButton sendButton;
     private ListView messagesListView;
+
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(getApplicationContext(), "ANON RECEIVER GOT BROADCAST", Toast.LENGTH_SHORT).show();
+            loadUnshownMessagesAndScroll();
+        }
+    };
 
     //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
     private BaseAdapter adapter;
@@ -36,6 +45,8 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        context.runo
 
         currentMessage  = (EditText)findViewById(R.id.current_message_edittext);
         sendButton = (ImageButton) findViewById(R.id.send_message_button);
@@ -54,32 +65,121 @@ public class ChatActivity extends AppCompatActivity {
                                           @Override
                                           public void onClick(View view) {
                                               String currentMsg = currentMessage.getText().toString();
-                                              if (currentMsg.toLowerCase().contains("test")) {
-                                                  addCounterpartyMessage(currentMsg);
-                                              } else {
-                                                  addCurrentUserMessage(currentMsg);
-                                              }
                                               currentMessage.setText("");
+
+                                              displayAndSaveCurrentUserMessage(currentMsg);
+                                              adapter.notifyDataSetChanged();
+                                              scrollToBottomOfChat();
                                           }
                                       });
+
+        loadMessages();
     }
 
-    private void addCurrentUserMessage(String msg) {
-        addMessage(msg, MessageType.SENT_BY_ME);
+    @Override
+    public void onResume() {
+        super.onResume();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(MyGcmListenerService.NEW_MESSAGE_BROADCAST);
+        registerReceiver(receiver, filter);
+
+        adapter.notifyDataSetChanged();
+        scrollToBottomOfChat();
+
     }
 
-    private void addCounterpartyMessage(String msg) {
-        addMessage(msg, MessageType.SENT_BY_OTHER);
+    int highestIdOfMessageShown = 0;
+
+    private void loadMessages() {
+        SharedPreferences sharedPrefsMsgs = getSharedPreferences(MyGcmListenerService.SHARED_PREFS_MSGS, Context.MODE_PRIVATE);
+        SharedPreferences sharedPrefsSenders = getSharedPreferences(MyGcmListenerService.SHARED_PREFS_SENDERS, Context.MODE_PRIVATE);
+
+        Map<String, ?> allMsgs = sharedPrefsMsgs.getAll();
+        Map<String, ?> allSenders = sharedPrefsSenders.getAll();
+
+        for (int i = 0; i < allMsgs.size(); i++) {
+            String index = Integer.valueOf(i).toString();
+            String sender = (String)allSenders.get(index);
+            String msg = (String)allMsgs.get(index);
+
+            Log.i("CATALANT", "loading message #" + index + "...");
+            displayMessage(msg, sender);
+        }
+
+        updateReadMessageCount();
     }
 
-    private void addMessage(String msg, MessageType messageType) {
+    private void updateReadMessageCount() {
+        int totalMsgs = getSharedPreferences(MyGcmListenerService.SHARED_PREFS_MSGS, Context.MODE_PRIVATE).getAll().size() - 1;
+        if (totalMsgs > highestIdOfMessageShown) {
+            highestIdOfMessageShown = totalMsgs;
+        }
+    }
+
+    private void loadUnshownMessagesAndScroll() {
+        SharedPreferences sharedPrefsMsgs = getSharedPreferences(MyGcmListenerService.SHARED_PREFS_MSGS, Context.MODE_PRIVATE);
+        SharedPreferences sharedPrefsSenders = getSharedPreferences(MyGcmListenerService.SHARED_PREFS_SENDERS, Context.MODE_PRIVATE);
+
+        int highestExistingMessageId = sharedPrefsMsgs.getAll().size() - 1;
+        Log.i("CATALANT", "I see a total of " + highestExistingMessageId + " messages in storage" );
+        Log.i("CATALANT", "highestExistingMessageId seen: " + highestIdOfMessageShown );
+        int numOfNewMessages = highestExistingMessageId - highestIdOfMessageShown;
+
+        // if (at first) there were 5 messages, highestId is 4
+        // then a new message arrives...
+        // now there are 6 total messages, and we need to update if highestIdOfMessageShown is not 5 (or, total messages - 1)
+        if (numOfNewMessages > 0) {
+            Log.i("CATALANT", "we have " + numOfNewMessages + " new/unread messages we must render.");
+        } else {
+            Log.i("CATALANT", "no new messages!");
+        }
+
+        for (int i = (highestExistingMessageId); i <= highestExistingMessageId; i++) {
+            Log.i("CATALANT", "rendering message " + i);
+            String message = sharedPrefsMsgs.getString(Integer.toString(i), null);
+            String sender = sharedPrefsSenders.getString(Integer.toString(i), null);
+
+            displayMessage(message, sender);
+        }
+        adapter.notifyDataSetChanged();
+        updateReadMessageCount();
+        scrollToBottomOfChat();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
+    }
+
+    // save the message, then add it to the local view
+    private void displayAndSaveCurrentUserMessage(String msg) {
+        MyGcmListenerService.addMessageToStorage(this, msg, "ME");
+        updateReadMessageCount();
+        displayMessage(msg, MessageType.SENT_BY_ME);
+    }
+
+    private void displayMessage(String msg, String sender) {
+        if (sender != null && sender.equals("ME")) {
+            displayMessage(msg, MessageType.SENT_BY_ME);
+        } else if (sender != null) {
+            displayMessage(msg, MessageType.SENT_BY_OTHER);
+        } else {
+            // umm warn or something?
+            displayMessage(msg, MessageType.SENT_BY_OTHER);
+        }
+    }
+
+    private void displayMessage(String msg, MessageType messageType) {
         if (msg == null || msg.length() < 1 || messageType == null) { return; }
+
         ChatMessage chatMessage = new ChatMessage();
         chatMessage.messageType = messageType;
         chatMessage.chatMessage = msg;
         chatMessages.add(chatMessage);
-        adapter.notifyDataSetChanged();
-        messagesListView.smoothScrollToPosition(adapter.getCount()-1);
     }
 
+    private void scrollToBottomOfChat() {
+        messagesListView.smoothScrollToPosition(adapter.getCount()-1);
+    }
 }
