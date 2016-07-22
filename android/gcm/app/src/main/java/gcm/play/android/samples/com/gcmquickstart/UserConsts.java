5def82e8acf91ca6a893aed0ec3945e02b9d6e43
Mark Roper <mroper@hourlynerd.com>
2016-07-21 21:00:11 -0400

    Removes userconst

--- android/gcm/app/src/main/java/gcm/play/android/samples/com/gcmquickstart/UserConsts.java
+++
@@ -1,9 +0,0 @@
-package gcm.play.android.samples.com.gcmquickstart;
-
-/**
- * Created by mark on 7/21/16.
- */
-public class UserConsts {
-    //TODO: HACK - this needs to be resolved as part of login...
-    public static final String DEFAULT_USER = "Mark";
-}
